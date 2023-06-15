fa78fae76a650c4cfa89b952a6ecee4a49cebdc0
Dave Nash <davidjnash2@gmail.com>
2023-06-15 08:49:32 -0500

    intialized setup, added garden database and plant table; on indexjs, removed starting plant array, added saga imports, and started builing base GET, POST, and DELETE generator functions-sagas, as well as rootSaga

--- src/index.js
+++ src/index.js
@@ -5,12 +5,58 @@ import { Provider } from 'react-redux';
 
 import App from './App';
 
-// this startingPlantArray should eventually be removed
-// const startingPlantArray = [
-//   { id: 1, name: 'Rose' },
-//   { id: 2, name: 'Tulip' },
-//   { id: 3, name: 'Oak' }
-// ];
+import createSagaMiddleware from 'redux-saga';
+import { takeLatest, put } from 'redux-saga/effects';
+import axios from 'axios';
+
+
+function* fetchPlants(action) {
+  try {
+      console.log('fetchPlants dinged:', action);
+      // hold response from server in plantsResponse
+      const plantsResponse = yield axios.get('/api/plant');
+      // after server responds, then this generator function/saga continues
+      console.log('fetchPlants response.data is:', plantsResponse.data);
+      yield put({ type: 'SET_PLANT_LIST', payload: plantsResponse.data })
+  } catch (error) {
+      console.log('Error with fetchPlants!', error);
+  }
+}
+
+function* postPlant(action) {
+  try {
+      // send axios post to server with payload
+      yield axios.post('/api/plant', action.payload);
+      console.log('postPlant payload is:', action.payload);
+      // trigger fetchPlants saga
+      yield put({ type: 'FETCH_PLANTS' });
+  } catch (error) {
+      console.log('Error POST-ing plant', error);
+  }
+}
+
+function* deletePlant(action) {
+  try {
+      // send axios post to server with payload
+      yield axios.delete(`/api/plant${action.payload}`);
+      console.log('deletePlant payload is:', action.payload);
+      // trigger fetchPlants saga
+      yield put({ type: 'FETCH_PLANTS' });
+  } catch (error) {
+      console.log('Error DELETE-ing plant', error);
+  }
+}
+
+
+// Create the rootSaga generator function
+function* rootSaga() {
+  yield takeLatest('FETCH_PLANTS', fetchPlants);
+  yield takeLatest('ADD_PLANT', postPlant);
+  yield takeLatest('DELETE_PLANT', deletePlant);
+}
+
+
+
 
 const plantList = (state = [], action) => {
   switch (action.type) {
